d9e6ba000c8ca5c8e56fe3df3780b78aafac58a5
biostrucx <u2651191@uel.ac.uk>
2025-06-17 12:51:10 +0100

    Import Navbar component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-// App.jsx será insertado después
+// src/App.jsx
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Aquí agregaremos luego las secciones Hero, Our Mission, etc. */}
+    </div>
+  );
+}
+
+export default App;
