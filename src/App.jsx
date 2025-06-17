4a0fb5dcbcfdf8af76f7b0ae95a1d73dd7662278
biostrucx <u2651191@uel.ac.uk>
2025-06-17 22:41:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // src/App.jsx
 import React from "react";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 
 function App() {
-return (
-  <>
-    <Navbar />
-    <HeroSection />
-  </>
-);
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
 }
 
 export default App;
+
