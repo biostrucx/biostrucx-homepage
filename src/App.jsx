22b88db4bdbf0e816be2691028045ce25c078384
biostrucx <u2651191@uel.ac.uk>
2025-06-20 04:53:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 
 function App() {
-  return (
-    <div className="bg-black text-white min-h-screen">
-      <Navbar />
-      <HeroSection />
-      {/* Aquí agregaremos luego la sección Our Mission, etc. */}
-    </div>
-  );
+return (
+  <div className="bg-black text-white min-h-screen">
+    <Navbar />
+    <LoginPage />
+    <HeroSection />
+    {/* Aquí agregaremos luego la sección Our Mission, etc. */}
+  </div>
+);
 }
 
 export default App;
